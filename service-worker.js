69260a59c64acf3b6e63abb0d6138419d30bf2b8
kuame225 <kuame225@gmail.com>
2026-08-17 11:46:14 +0000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "dabakala-432";
-const ASSETS = ["./manifest.json", "./icon-192.png", "./icon-512.png", "./banner-hero.jpg"].concat(["./app.O4J3SZ2K.js","./app.5MQKNDBK.css"]);
+const CACHE = "dabakala-554";
+const ASSETS = ["./manifest.json", "./icon-192.png", "./icon-512.png", "./banner-hero.jpg"].concat(["./app.K55S4RPW.js","./app.5MQKNDBK.css"]);
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
